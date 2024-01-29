55d953c0c68329928cd371a9faab94ad2c1d9f0f
Shivansh Yadav <yadavshivansh@gmail.com>
2024-01-29 20:42:45 +0530

    cloth segment using mediapipe

--- app/page.js
+++ app/page.js
@@ -1,113 +1,120 @@
-import Image from "next/image";
+"use client"
+import { useRef } from 'react';
+import { FilesetResolver, ImageSegmenter } from '@mediapipe/tasks-vision';
+
+const legendColors = [
+  [255, 197, 0, 255], // Vivid Yellow
+  [128, 62, 117, 255], // Strong Purple
+  [255, 104, 0, 255], // Vivid Orange
+  [166, 189, 215, 255], // Very Light Blue
+  [193, 0, 32, 255], // Vivid Red
+  [206, 162, 98, 255], // Grayish Yellow
+  [129, 112, 102, 255], // Medium Gray
+  [0, 125, 52, 255], // Vivid Green
+  [246, 118, 142, 255], // Strong Purplish Pink
+  [0, 83, 138, 255], // Strong Blue
+  [255, 112, 92, 255], // Strong Yellowish Pink
+  [83, 55, 112, 255], // Strong Violet
+  [255, 142, 0, 255], // Vivid Orange Yellow
+  [179, 40, 81, 255], // Strong Purplish Red
+  [244, 200, 0, 255], // Vivid Greenish Yellow
+  [127, 24, 13, 255], // Strong Reddish Brown
+  [147, 170, 0, 255], // Vivid Yellowish Green
+  [89, 51, 21, 255], // Deep Yellowish Brown
+  [241, 58, 19, 255], // Vivid Reddish Orange
+  [35, 44, 22, 255], // Dark Olive Green
+  [0, 161, 194, 255] // Vivid Blue
+];
 
 export default function Home() {
-  return (
+ const imgRef = useRef(null);
+ const canvasRef = useRef(null);
+
+ const createImageSegmenter = async () => {
+   const vision = await FilesetResolver.forVisionTasks('https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm');
+
+   const imageSegmenter = await ImageSegmenter.createFromOptions(vision, {
+     baseOptions: {
+      //  modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/image_segmenter/selfie_segmenter/float16/latest/selfie_segmenter.tflite',
+        modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/image_segmenter/selfie_multiclass_256x256/float32/latest/selfie_multiclass_256x256.tflite',
+       delegate: "CPU"
+     },
+     outputCategoryMask: true,
+     outputConfidenceMasks: false,
+     runningMode: "IMAGE"
+   });
+
+   if (imgRef.current && canvasRef.current) {
+     imageSegmenter.segment(imgRef.current, (result) => {
+        const canvas = document.getElementById("canvas");
+        const img = document.getElementById("img");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        const ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+
+        const { width, height } = result.categoryMask;
+        let imageData = ctx.getImageData(0, 0, width, height).data;
+
+        canvas.width = width;
+        canvas.height = height;
+
+        const mask = result.categoryMask.getAsUint8Array();
+
+        for (let i in mask) {
+          const legendColor = legendColors[mask[i] % legendColors.length]; // getting the color set for the segmented part
+          // console.log(mask[i] % legendColors.length);
+          // We divide by 2 to avegrage out the pixel color values from the original image and the legend color
+          // imageData[i * 4] = (legendColor[0] + imageData[i * 4]) / 2; // Red 
+          // imageData[i * 4 + 1] = (legendColor[1] + imageData[i * 4 + 1]) / 2; // Green
+          // imageData[i * 4 + 2] = (legendColor[2] + imageData[i * 4 + 2]) / 2; // Blue
+          // imageData[i * 4 + 3] = (legendColor[3] + imageData[i * 4 + 3]) / 2; // Alpha
+          if (mask[i] % legendColors.length == 4) {
+            mask[i] = 1;
+
+            imageData[i * 4] = 255; // Red 
+            imageData[i * 4 + 1] = 255; // Green
+            imageData[i * 4 + 2] = 255; // Blue
+            imageData[i * 4 + 3] = 255; // Alpha
+
+            // imageData[i * 4] = legendColor[0]; // Red 
+            // imageData[i * 4 + 1] = legendColor[1]; // Green
+            // imageData[i * 4 + 2] = legendColor[2]; // Blue
+            // imageData[i * 4 + 3] = legendColor[3]; // Alpha
+          } else {
+            mask[i] = 0;
+            imageData[i * 4] = 0; // Red 
+            imageData[i * 4 + 1] = 0; // Green
+            imageData[i * 4 + 2] = 0; // Blue
+            imageData[i * 4 + 3] = 0; // Alpha
+          }
+        }
+
+        const uint8Array = new Uint8ClampedArray(imageData.buffer);
+        const dataNew = new ImageData(uint8Array, width, height);
+        ctx.putImageData(dataNew, 0, 0);
+     });
+   }
+ }
+
+ return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <p>Segment Any Image</p>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="flex justify-between">
+        <img
+          src="/img.jpg"
+          id='img'
+          ref={imgRef}
+          alt="Img"
         />
+
+        <canvas ref={canvasRef} id="canvas"></canvas>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <button onClick={createImageSegmenter}>Segment Image</button>
     </main>
-  );
+ );
 }
